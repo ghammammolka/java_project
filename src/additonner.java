c5c4cf0724aa4e74b60b4cb0c25122e810283935
ghammammolka <147916215+ghammammolka@users.noreply.github.com>
2024-04-22 09:34:16 +0100

    Update additonner.java

--- src/additonner.java
+++ src/additonner.java
@@ -17,19 +17,25 @@ public class additonner {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-         Scanner scanner = new Scanner(System.in);
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        System.out.print("entrer le premier nombre :");
+        System.out.print("Entrer le premier nombre : ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Ce n'est pas un entier. Veuillez entrer un entier.");
+            scanner.next(); // Consommer l'entrée invalide
+        }
         int n1 = scanner.nextInt();
         
-        System.out.print("entrer le deuxiÃ¨me nombre :");
+        System.out.print("Entrer le deuxième nombre : ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Ce n'est pas un entier. Veuillez entrer un entier.");
+            scanner.next(); // Consommer l'entrée invalide
+        }
         int n2 = scanner.nextInt();
         
         int somme = n1 + n2;
 
-        System.out.println("La somme est :" + somme);
+        System.out.println("La somme est : " + somme);
     }
-    
 }
