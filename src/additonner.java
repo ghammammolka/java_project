5d9d5a4dac8dca20f825c6aab63b46b913ca07a8
ghammammolka <147916215+ghammammolka@users.noreply.github.com>
2024-04-23 16:34:44 +0100

    Update additonner.java

--- src/additonner.java
+++ src/additonner.java
@@ -1,40 +1,19 @@
-import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author User
- */
-import java.util.Scanner;
 public class additonner {
 
-    /**
-     * @param args the command line arguments
-     */
-   public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Entrer le premier nombre : ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Ce n'est pas un entier. Veuillez entrer un entier.");
-            scanner.next(); // Consommer l'entrée invalide
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Veuillez fournir deux nombres en argument.");
+            return; // Quitte le programme si les arguments ne sont pas fournis
         }
-        int n1 = scanner.nextInt();
-        
-        System.out.print("Entrer le deuxième nombre : ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Ce n'est pas un entier. Veuillez entrer un entier.");
-            scanner.next(); // Consommer l'entrée invalide
-        }
-        int n2 = scanner.nextInt();
-        
-        int somme = n1 + n2;
 
-        System.out.println("La somme est : " + somme);
+        // Convertit les arguments en entiers
+        try {
+            int n1 = Integer.parseInt(args[0]);
+            int n2 = Integer.parseInt(args[1]);
+            int somme = n1 + n2;
+            System.out.println("La somme est : " + somme);
+        } catch (NumberFormatException e) {
+            System.out.println("Les arguments ne sont pas des nombres valides.");
+        }
     }
 }
